37430e745e0bce3d291db11a754e5c892dd9fad5
Peter Makafan <makafanpeter@gmail.com>
2016-03-01 18:19:59 +0100

    First commit

---
+++ static/js/init.js
@@ -0,0 +1,376 @@
+(function($){
+  $(function(){
+
+    $('.button-collapse').sideNav();
+
+  }); // end of document ready
+})(jQuery); // end of jQuery name space
+var appliances = [
+  {
+    "wattage": "1000",
+    "name": "1 Hp"
+  },
+  {
+    "wattage": "180",
+    "name": "Box Fan"
+  },
+  {
+    "wattage": "750",
+    "name": "Projector"
+  },
+  {
+    "wattage": "3",
+    "name": "Electric Clock"
+  },
+  {
+    "wattage": "50",
+    "name": "Security Camera"
+  },
+  {
+    "wattage": "3000",
+    "name": "3 Hp"
+  },
+  {
+    "wattage": "1100",
+    "name": "Clothes Iron"
+  },
+  {
+    "wattage": "1200",
+    "name": "Hair Dryer"
+  },
+  {
+    "wattage": "75",
+    "name": "Sewing Machine"
+  },
+  {
+    "wattage": "12",
+    "name": "Wireless Router"
+  },
+  {
+    "wattage": "25",
+    "name": "Portable Radio"
+  },
+  {
+    "wattage": "6000",
+    "name": "Pumping Machine 1Hp"
+  },
+  {
+    "wattage": "9068",
+    "name": "Pumping Machine 15Hp"
+  },
+  {
+    "wattage": "12000",
+    "name": "Pumping Machine 2Hp"
+  },
+  {
+    "wattage": "668",
+    "name": "Shredder"
+  },
+  {
+    "wattage": "2000",
+    "name": "Ups 2000"
+  },
+  {
+    "wattage": "70",
+    "name": "Ceiling Fan"
+  },
+  {
+    "wattage": "18",
+    "name": "18 Watt Energy"
+  },
+  {
+    "wattage": "23",
+    "name": "23 Watt Energy"
+  },
+  {
+    "wattage": "50",
+    "name": "50 Watt Halogen"
+  },
+  {
+    "wattage": "90",
+    "name": "90 Watt Halogen"
+  },
+  {
+    "wattage": "60",
+    "name": "60 Watt Bulb"
+  },
+  {
+    "wattage": "75",
+    "name": "75 Watt Bulb"
+  },
+  {
+    "wattage": "100",
+    "name": "100 Watt Bulb"
+  },
+  {
+    "wattage": "11",
+    "name": "11 Watt Energy"
+  },
+  {
+    "wattage": "30",
+    "name": "Dvd Player"
+  },
+  {
+    "wattage": "150",
+    "name": "Play Station"
+  },
+  {
+    "wattage": "6",
+    "name": "Cctv Energy"
+  },
+  {
+    "wattage": "300",
+    "name": "Stereo"
+  },
+  {
+    "wattage": "200",
+    "name": "200 Wattbulb"
+  },
+  {
+    "wattage": "250",
+    "name": "250 Wattbulb"
+  },
+  {
+    "wattage": "500",
+    "name": "Home Theatre"
+  },
+  {
+    "wattage": "28",
+    "name": "Decoder"
+  },
+  {
+    "wattage": "440",
+    "name": "Freezer"
+  },
+  {
+    "wattage": "350",
+    "name": "Refridgerator"
+  },
+  {
+    "wattage": "1500",
+    "name": "Microwave Oven"
+  },
+  {
+    "wattage": "550",
+    "name": "Water Dispenser"
+  },
+  {
+    "wattage": "1000",
+    "name": "Deep Fryer"
+  },
+  {
+    "wattage": "3500",
+    "name": "Oven"
+  },
+  {
+    "wattage": "1500",
+    "name": "Electric Frying Pan"
+  },
+  {
+    "wattage": "1500",
+    "name": "Microwave"
+  },
+  {
+    "wattage": "1100",
+    "name": "Toaster"
+  },
+  {
+    "wattage": "200",
+    "name": "Slow Cooker"
+  },
+  {
+    "wattage": "1000",
+    "name": "Ups1000"
+  },
+  {
+    "wattage": "220",
+    "name": "Lcd Tv"
+  },
+  {
+    "wattage": "1",
+    "name": "Phone Charger"
+  },
+  {
+    "wattage": "400",
+    "name": "Plasma Tv"
+  },
+  {
+    "wattage": "4000",
+    "name": "Water Heater"
+  },
+  {
+    "wattage": "200",
+    "name": "Extractor Fan"
+  },
+  {
+    "wattage": "2500",
+    "name": "212 Hp"
+  },
+  {
+    "wattage": "2000",
+    "name": "2 Hp"
+  },
+  {
+    "wattage": "1500",
+    "name": "Coffee Machine"
+  },
+  {
+    "wattage": "385",
+    "name": "Blender"
+  },
+  {
+    "wattage": "650",
+    "name": "Vacuum Cleaner"
+  },
+  {
+    "wattage": "1200",
+    "name": "Dish Washer"
+  },
+  {
+    "wattage": "5000",
+    "name": "Clothes Dryer"
+  },
+  {
+    "wattage": "300",
+    "name": "Handheld Vacuum"
+  },
+  {
+    "wattage": "750",
+    "name": "Kettle"
+  },
+  {
+    "wattage": "1200",
+    "name": "Washing Machine"
+  },
+  {
+    "wattage": "40",
+    "name": "40 Watt Bulb"
+  },
+  {
+    "wattage": "20",
+    "name": "20 Watt Bulb"
+  },
+  {
+    "wattage": "25",
+    "name": "Laptop"
+  },
+  {
+    "wattage": "150",
+    "name": "Desktop Computer"
+  },
+  {
+    "wattage": "200",
+    "name": "Standard Colour Tv"
+  },
+  {
+    "wattage": "184",
+    "name": "Photocopier"
+  },
+  {
+    "wattage": "50",
+    "name": "Inkjet Printer"
+  },
+  {
+    "wattage": "350",
+    "name": "Laserjet Printer"
+  },
+  {
+    "wattage": "75",
+    "name": "Crt Monitor"
+  },
+  {
+    "wattage": "500",
+    "name": "12 Hp"
+  },
+  {
+    "wattage": "35",
+    "name": "Lcd Monitor"
+  },
+  {
+    "wattage": "1500",
+    "name": "15 Hp"
+  },
+  {
+    "wattage": "650",
+    "name": "Ups 650"
+  }
+].sort(function(a,b){
+if (a.name > b.name)
+        return 1;
+    if (a.name < b.name)
+        return -1;
+    return 0;});
+
+
+
+function formatCurrency(value) {
+  return value.toFixed(2);
+}
+
+var Appliance = function(p) {
+  var self = this;
+  self.appliance = ko.observable(p.appliance);
+  self.hoursUsedPerDay = ko.observable(p.hoursUsedPerDay);
+  self.wattage = ko.observable(p.wattage);
+  self.kWh = ko.computed(function() {
+    return self.appliance() ? (parseFloat("0" + self.hoursUsedPerDay(), 10) * parseFloat("0" + self.appliance().wattage, 10)) / 1000 : 0;
+  });
+
+  return self;
+};
+
+var Calculator = function() {
+  var self = this;
+  // Put one line in by default
+  self.lines = ko.observableArray([new Appliance({
+    "hoursUsedPerDay": 1
+  })]);
+  self.tariffRate = ko.observable(19);
+  self.kwhPerDay = ko.computed(function() {
+    var total = 0;
+    $.each(self.lines(), function() {
+      total += this.kWh()
+    })
+    return total.toFixed(2);
+  });
+
+  self.kwhPerMonth = ko.computed(function() {
+    var today = new Date();
+    var year = today.getFullYear();
+    var month = today.getMonth() + 1;
+    var daysInMonth = new Date(year, month, 1, -1).getDate();
+    return (self.kwhPerDay() * daysInMonth).toFixed(2);
+  });
+
+  self.kwhPerYear = ko.computed(function() {
+    var today = new Date();
+    var year = today.getFullYear();
+    var daysInYear = year % 4 == 0 ? 366 : 365
+    return (self.kwhPerDay() * daysInYear).toFixed(2);
+  });
+
+  // Operations
+  self.addLine = function() {
+    self.lines.push(new Appliance({
+      "hoursUsedPerDay": 1
+    }))
+  };
+  self.removeLine = function(line) {
+    self.lines.remove(line)
+  };
+  self.save = function() {
+    var dataToSave = $.map(self.lines(), function(line) {
+      return line.appliance() ? {
+        productName: line.appliance(),
+        quantity: line.hoursUsedPerDay()
+      } : undefined
+    });
+    alert("Could now send this to server: " + JSON.stringify(dataToSave));
+  };
+
+  self.render = function(){
+   $('select').material_select();
+  }
+};
+
+ko.applyBindings(new Calculator());
